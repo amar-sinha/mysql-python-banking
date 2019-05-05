83d696dc159257a554be1a5ff4beda2d5d4537e1
amar-sinha <amar.sinha@uconn.edu>
2019-05-04 20:52:08 -0400

    add mysql-python-banking-db.sql file for reference

---
+++ mysql-python-banking-db.sql
@@ -0,0 +1,19 @@
+--
+-- Database: cse4701s19_project2
+--
+CREATE DATABASE IF NOT EXISTS mysql_python_banking;
+USE mysql_python_banking;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table account
+--
+
+CREATE TABLE account (
+  account_no int(11) NOT NULL AUTO_INCREMENT,
+  name_on_account varchar(100) NOT NULL,
+  balance float NOT NULL DEFAULT '0',
+  account_open_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (account_no)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
